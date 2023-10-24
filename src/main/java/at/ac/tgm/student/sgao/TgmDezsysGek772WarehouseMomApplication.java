28e99982e01d67c826117e6bc4c4bf85e4f230bc
Simon Gao <simon.shubin@gmail.com>
2023-10-24 16:53:53 +0200

    initial commit

---
+++ src/main/java/at/ac/tgm/student/sgao/TgmDezsysGek772WarehouseMomApplication.java
@@ -0,0 +1,13 @@
+package at.ac.tgm.student.sgao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgmDezsysGek772WarehouseMomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgmDezsysGek772WarehouseMomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ac/tgm/student/sgao/TgmDezsysGek772WarehouseMomApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.tgm.student.sgao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgmDezsysGek772WarehouseMomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
