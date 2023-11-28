dae1a69dc99ea7d9e3c53f890e5f0ca5e6bbb8b7
Simon Gao <simon.shubin@gmail.com>
2023-11-28 08:54:14 +0100

    deleted old code

--- src/main/java/at/ac/tgm/student/sgao/WarehouseCenterApplication.java
+++ src/main/java/at/ac/tgm/student/sgao/WarehouseCenterApplication.java
@@ -3,8 +3,6 @@ package at.ac.tgm.student.sgao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class WarehouseCenterApplication {
 
@@ -13,30 +11,3 @@ public class WarehouseCenterApplication {
 	}
 }
 
-
-// @SpringBootApplication
-// public class Warehouse2Application {
-//
-//     public static Sender sender;
-//     public static Receiver receiver;
-//
-//     public static boolean main = false;
-//
-//     public static void main(String[] args) throws InterruptedException {
-//         SpringApplication app = new SpringApplication(Warehouse2Application.class);
-//         app.setDefaultProperties(Collections.singletonMap("server.port", Integer.valueOf(args[1])));
-//         app.run(args);
-//
-//         String topic = args[0];
-//
-//         if(topic.equals("main")) {
-//             main = true;
-//
-//             receiver = new Receiver("war1", true);
-//             sender = new Sender("war1");
-//         }else {
-//             receiver = new Receiver(topic, false);
-//             sender = new Sender(topic);
-//         }
-//     }
-// }
