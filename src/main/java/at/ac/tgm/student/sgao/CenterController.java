30e81e7c9ffc882d3c0ac23dcd527cb1df183ca1
Simon Gao <simon.shubin@gmail.com>
2023-11-28 08:55:31 +0100

    added imports to data

--- src/main/java/at/ac/tgm/student/sgao/CenterController.java
+++ src/main/java/at/ac/tgm/student/sgao/CenterController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import at.ac.tgm.student.sgao.data.WarehouseData;
+
 import java.util.Random;
 
 /**
--- src/main/java/at/ac/tgm/student/sgao/Sender.java
+++ src/main/java/at/ac/tgm/student/sgao/Sender.java
@@ -6,6 +6,8 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import at.ac.tgm.student.sgao.data.WarehouseData;
+
 public class Sender {
 
     Logger logger = LoggerFactory.getLogger(Sender.class);
